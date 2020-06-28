75c747ba09de01435ebe42162389008771a37bd1
Diego97-webdev <shizusenpai97@gmail.com>
2020-06-28 20:03:14 -0300

    agregado los listeners del filtro y tags, agregada la funcionalidad del span del titulo

--- js/app.js
+++ js/app.js
@@ -1,1 +1,46 @@
 /*VISUAL ANTALITICO Y MINIMALISTA*/
+const botonCategorias = document.querySelector(".clickeable");
+const contenedorTags = document.querySelector(".contenedor-categorias");
+const tags = document.querySelectorAll(".categoria");
+const imagenes = document.querySelectorAll(".imagenes");
+const spanTitulo = document.querySelector(".efecto-titulo");
+
+//EVENT LISTENER CATEGORIAS
+
+botonCategorias.addEventListener("click", (e) => {
+  e.preventDefault;
+  contenedorTags.classList.toggle("display");
+});
+
+//EVENT LISTENER FILTRO
+
+tags.forEach((tag) => {
+  tag.addEventListener("click", (e) => {
+    const valorData = e.target.dataset.filter;
+    imagenes.forEach((imagen) => {
+      if (valorData == "todos") {
+        imagen.style.display = "block";
+      } else if (valorData == imagen.dataset.item) {
+        imagen.style.display = "block";
+      } else {
+        imagen.style.display = "none";
+      }
+    });
+  });
+});
+
+//SET TIMER CAMBIO DE PALABRA DEL TITULO
+let contador = 0;
+
+function cambiarNombre() {
+  const palabras = ["analiticos", "minimalistas", "visuales"];
+
+  spanTitulo.textContent = palabras[contador];
+
+  contador++;
+  if (contador > 2) {
+    contador = 0;
+  }
+}
+
+setInterval(cambiarNombre, 2000);

82e3bab35f4a72158351bdae7978672fe5123c1c
Diego97-webdev <shizusenpai97@gmail.com>
2020-07-01 19:30:31 -0300

    optimizado el codigo css y js con comentarios, ordenamiento de las secciones y eliminacion de declaraciones repetidas

--- js/app.js
+++ js/app.js
@@ -1,95 +1,104 @@
-/*VISUAL ANTALITICO Y MINIMALISTA*/
-const botonCategorias = document.querySelector(".clickeable");
-const contenedorTags = document.querySelector(".contenedor-categorias");
-const tags = document.querySelectorAll(".categoria");
-const contenedorDeImagenes = document.querySelectorAll(".imagenes");
-const imagenesPrincipales = document.querySelectorAll(".imagenes-principales");
-const spanTitulo = document.querySelector(".efecto-titulo");
-const btnCerrarFlotante = document.querySelector(".btn-cerrar");
-const imgFlotante = document.querySelector(".img-flotante");
-const btnIzqFlotante = document.querySelector(".btn-izquierda");
-const btnDerFlotante = document.querySelector(".btn-derecha");
-const contenedorFlotante = document.querySelector(
-  ".contenedor-imagen-flotante"
-);
-const botonHover = document.querySelectorAll(".btn-hover");
-const hoverImagenes = document.querySelectorAll(".hover-imgenes");
+(function () {
+  //IIFE PARA NO CONTAMINAR EL SCOPE GLOBAL
 
-//EVENT LISTENER CATEGORIAS
+  //SECCION HEADER
+  const spanTitulo = document.querySelector(".efecto-titulo");
 
-botonCategorias.addEventListener("click", (e) => {
-  e.preventDefault;
-  contenedorTags.classList.toggle("display");
-});
+  //SECCION FILTRO
+  const botonCategorias = document.querySelector(".clickeable");
+  const contenedorTags = document.querySelector(".contenedor-categorias");
+  const tags = document.querySelectorAll(".categoria");
 
-//EVENT LISTENER FILTRO
+  //SECCION IMAGENES
+  const contenedorDeImagenes = document.querySelectorAll(".imagenes");
+  const imagenesPrincipales = document.querySelectorAll(
+    ".imagenes-principales"
+  );
+  const botonHover = document.querySelectorAll(".btn-hover");
+  const hoverImagenes = document.querySelectorAll(".hover-imgenes");
 
-tags.forEach((tag) => {
-  tag.addEventListener("click", (e) => {
-    const valorData = e.target.dataset.filter;
-    contenedorDeImagenes.forEach((imagen) => {
-      if (valorData == "todos") {
-        imagen.style.display = "flex";
-      } else if (valorData == imagen.dataset.item) {
-        imagen.style.display = "flex";
-      } else {
-        imagen.style.display = "none";
-      }
+  //SECCION FLOTANTE
+  const btnCerrarFlotante = document.querySelector(".btn-cerrar");
+  const imgFlotante = document.querySelector(".img-flotante");
+  const btnIzqFlotante = document.querySelector(".btn-izquierda");
+  const btnDerFlotante = document.querySelector(".btn-derecha");
+  const contenedorFlotante = document.querySelector(
+    ".contenedor-imagen-flotante"
+  );
+
+  //SET INTERVAL CAMBIO DE PALABRA DEL TITULO
+  let contador = 0;
+
+  const cambiarNombre = () => {
+    const palabras = ["analiticos", "minimalistas", "visuales"];
+
+    spanTitulo.textContent = palabras[contador];
+
+    contador++;
+
+    if (contador > 2) {
+      contador = 0;
+    }
+  };
+  setInterval(cambiarNombre, 2000);
+
+  //EVENT LISTENERS SECCION FILTRO
+  botonCategorias.addEventListener("click", (e) => {
+    e.preventDefault;
+    contenedorTags.classList.toggle("display");
+  });
+
+  tags.forEach((tag) => {
+    tag.addEventListener("click", (e) => {
+      const valorData = e.target.dataset.filter;
+      contenedorDeImagenes.forEach((imagen) => {
+        if (valorData == "todos") {
+          imagen.style.display = "flex";
+        } else if (valorData == imagen.dataset.item) {
+          imagen.style.display = "flex";
+        } else {
+          imagen.style.display = "none";
+        }
+      });
     });
   });
-});
 
-//SET INTERVAL CAMBIO DE PALABRA DEL TITULO
-let contador = 0;
-
-function cambiarNombre() {
-  const palabras = ["analiticos", "minimalistas", "visuales"];
-
-  spanTitulo.textContent = palabras[contador];
-
-  contador++;
-  if (contador > 2) {
-    contador = 0;
-  }
-}
-setInterval(cambiarNombre, 2000);
-
-//EVENT LISTENERS DEL FLOTANTE
-let listaDeImagenes = [];
-
-let contadorImagenes = 0;
-
-imagenesPrincipales.forEach((imagen) => {
-  listaDeImagenes.push(imagen.src);
-});
-
-btnCerrarFlotante.addEventListener("click", () => {
-  contenedorFlotante.style.display = "none";
-});
-
-botonHover.forEach((boton) => {
-  boton.addEventListener("click", (e) => {
-    let botonClickeado = e.target.parentNode.parentNode.childNodes[1].src; //Obtener el src de la imagen a la que se le hace click para pasarselo a la imagen flotante,
-    contenedorFlotante.style.display = "flex";
-    imgFlotante.src = botonClickeado;
-    contadorImagenes = listaDeImagenes.indexOf(imgFlotante.src); //obtener el numero de index de la imagen que fue selecionada para pasarlo como referencia en los event listener de las flechas
+  //EVENT LISTENERS SECCION FLOTANTE
+  btnCerrarFlotante.addEventListener("click", () => {
+    contenedorFlotante.style.display = "none";
   });
-});
 
-//EVENT LISTENERS DE LOS BOTONES DEL FLOTANTE
+  let listaDeImagenes = []; //Array para guardar los src de todas las img
 
-btnIzqFlotante.addEventListener("click", () => {
-  contadorImagenes--;
-  if (contadorImagenes < 0) {
-    contadorImagenes = listaDeImagenes.length - 1;
-  }
-  imgFlotante.src = listaDeImagenes[contadorImagenes];
-});
+  let contadorImagenes = 0;
 
-btnDerFlotante.addEventListener("click", () => {
-  contadorImagenes++;
-  if (contadorImagenes > listaDeImagenes.length - 1) {
-    contadorImagenes = 0;
-  }
-  imgFlotante.src = listaDeImagenes[contadorImagenes];
-});
+  imagenesPrincipales.forEach((imagen) => {
+    listaDeImagenes.push(imagen.src); //Agregar todas los src de las img al array creado anteriormente
+  });
+
+  botonHover.forEach((boton) => {
+    boton.addEventListener("click", (e) => {
+      let botonClickeado = e.target.parentNode.parentNode.childNodes[1].src; //Obtener el src de la imagen a la que se le hace click para pasarselo a la imagen flotante
+      contenedorFlotante.style.display = "flex";
+      imgFlotante.src = botonClickeado;
+      contadorImagenes = listaDeImagenes.indexOf(imgFlotante.src); //obtener el numero de index de la imagen que fue selecionada para pasarlo como referencia en los event listener de las flechas
+    });
+  });
+
+  //Event listeners botones flotante
+  btnIzqFlotante.addEventListener("click", () => {
+    contadorImagenes--;
+    if (contadorImagenes < 0) {
+      contadorImagenes = listaDeImagenes.length - 1;
+    }
+    imgFlotante.src = listaDeImagenes[contadorImagenes];
+  });
+
+  btnDerFlotante.addEventListener("click", () => {
+    contadorImagenes++;
+    if (contadorImagenes > listaDeImagenes.length - 1) {
+      contadorImagenes = 0;
+    }
+    imgFlotante.src = listaDeImagenes[contadorImagenes];
+  });
+})();
